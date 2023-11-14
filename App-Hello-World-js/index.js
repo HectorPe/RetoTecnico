00b5eb1082d94699b0ecba92914e421a19f3f895
Hector Felipe Peñalva Torres <hpenalva@winempresas.pe>
2023-11-14 02:34:42 -0500

    Nueva pag2

--- App-Hello-World-js/index.js
+++ App-Hello-World-js/index.js
@@ -1,7 +1,2 @@
 console.log("Hello World!")
 console.log("Hola pedrito")
-console.log("Hola pedrito")
-console.log("Hola pedrito")
-console.log("Hola pedrito")
-console.log("Hola pedrito")
-console.log("Hola pedrito")
